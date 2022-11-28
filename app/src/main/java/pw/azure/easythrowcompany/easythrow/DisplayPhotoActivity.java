136e8bbd34a50eed7eedd7ed5d31d2f8e971f707
Jakub Maciejewski <jakubm55555@gmail.com>
2022-11-28 01:26:12 +0100

    FIRST working version, with detection on mobile application

--- app/src/main/java/pw/azure/easythrowcompany/easythrow/DisplayPhotoActivity.java
+++ app/src/main/java/pw/azure/easythrowcompany/easythrow/DisplayPhotoActivity.java
@@ -2,7 +2,6 @@ package pw.azure.easythrowcompany.easythrow;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
@@ -10,13 +9,17 @@ import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.ParseException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -42,7 +45,7 @@ public class DisplayPhotoActivity extends AppCompatActivity {
         ImageView photoIv = findViewById(R.id.capturedImageView);
         Bundle extras = getIntent().getExtras();
         Uri imageUri = Uri.parse(extras.getString("imageUri"));
-        //photoIv.setImageURI(imageUri);
+        photoIv.setImageURI(imageUri);
 
         ProgressBar progressBar = findViewById(R.id.uploadPb);
 
@@ -61,14 +64,14 @@ public class DisplayPhotoActivity extends AppCompatActivity {
         Bitmap bmp = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getHeight(), Bitmap.Config.RGB_565);
 
 
-        Canvas canvas = new Canvas(bmp);
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
-        paint.setColor(Color.RED);
-        canvas.drawBitmap(bitmap,0,0, null);
-        canvas.drawRect(0,0,100,100, paint);
-        photoIv.setImageBitmap(bmp);
+//        Canvas canvas = new Canvas(bmp);
+//        Paint paint = new Paint();
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setStrokeWidth(10);
+//        paint.setColor(Color.RED);
+//        canvas.drawBitmap(bitmap,0,0, null);
+//        canvas.drawRect(0,0,100,100, paint);
+//        photoIv.setImageBitmap(bmp);
 
 
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -101,6 +104,13 @@ public class DisplayPhotoActivity extends AppCompatActivity {
                             String myResponse = response.body().string();
                             System.out.println(myResponse);
                             System.out.println(imageUri);
+                            try {
+                                DrawRectanglesFromJSONResponse(myResponse, finalBitmap, photoIv);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
                             DisplayPhotoActivity.this.runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -130,27 +140,72 @@ public class DisplayPhotoActivity extends AppCompatActivity {
         });
     }
 
+    public static void DrawRectanglesFromJSONResponse(String response, Bitmap bitmap, ImageView imageView) throws IOException, ParseException, JSONException {
+
+        Bitmap bmp = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint();
+
+        canvas.drawBitmap(bitmap,0,0, null);
+
+        JSONObject jo = new JSONObject(response);
+
+        String filename = (String) jo.get("filename");
+
+        JSONArray boxes = jo.getJSONArray("boxes");
+
+        // iterating phoneNumbers
+
+        for (int i = 0; i < boxes.length(); i++) {
+            JSONObject box = boxes.getJSONObject(i).getJSONObject("box");
+            double topX = box.getDouble("topX");
+            double topY = box.getDouble("topY");
+            double bottomX = box.getDouble("bottomX");
+            double bottomY = box.getDouble("bottomY");
+            String label = boxes.getJSONObject(i).getString("label");
+            double score = boxes.getJSONObject(i).getDouble("score");
+
+            if(score >= 0.5){
+                int topXPixel = (int) (topX * bitmap.getWidth());
+                int topYPixel = (int) (topY * bitmap.getHeight());
+                int bottomXPixel = (int) (bottomX * bitmap.getWidth());
+                int bottomYPixel = (int) (bottomY * bitmap.getHeight());
+
+                int color;
+
+                if(label.equals("PlasticAndMetal")){
+                    color = Color.YELLOW;
+                } else if( label.equals("Mixed")){
+                    color = Color.BLACK;
+                } else if(label.equals("Glass")){
+                    color = Color.GREEN;
+                } else if(label.equals("Paper")){
+                    color = Color.BLUE;
+                } else if(label.equals("WasteCollectionPoint")){
+                    color = Color.RED;
+                } else {
+                    color = Color.MAGENTA;
+                }
+
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeWidth(10);
+                paint.setColor(color);
+
+                canvas.drawRect(topXPixel,topYPixel,bottomXPixel,bottomYPixel, paint);
+
+
+            }
+        }
+
+        imageView.setImageBitmap(bmp);
+
+    }
+
     public static Bitmap RotateBitmap(Bitmap source, float angle)
     {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-    }
-
-    public static String encodeTobase64(Bitmap image)
-    {
-        Bitmap immagex=image;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        immagex.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] b = baos.toByteArray();
-        String imageEncoded = Base64.encodeToString(b,Base64.DEFAULT);
-        return imageEncoded;
-    }
-
-    public static Bitmap decodeBase64(String input)
-    {
-        byte[] decodedByte = Base64.decode(input, 0);
-        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
     }
 
     @Override
